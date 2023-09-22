46687e2304d113479fed1d24fc901901d5921679
Cole-Alt-Del <57567614+Cole-Alt-Del@users.noreply.github.com>
2023-09-22 09:56:37 -0500

    Added detection capability, fetchdetect still needs testing.

---
+++ fetchdetect.py
@@ -0,0 +1,334 @@
+# YOLOv5 🚀 by Ultralytics, AGPL-3.0 license
+"""
+Run YOLOv5 detection inference on images, videos, directories, globs, YouTube, webcam, streams, etc.
+
+Usage - sources:
+    $ python detect.py --weights yolov5s.pt --source 0                               # webcam
+                                                     img.jpg                         # image
+                                                     vid.mp4                         # video
+                                                     screen                          # screenshot
+                                                     path/                           # directory
+                                                     list.txt                        # list of images
+                                                     list.streams                    # list of streams
+                                                     'path/*.jpg'                    # glob
+                                                     'https://youtu.be/LNwODJXcvt4'  # YouTube
+                                                     'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP stream
+
+Usage - formats:
+    $ python detect.py --weights yolov5s.pt                 # PyTorch
+                                 yolov5s.torchscript        # TorchScript
+                                 yolov5s.onnx               # ONNX Runtime or OpenCV DNN with --dnn
+                                 yolov5s_openvino_model     # OpenVINO
+                                 yolov5s.engine             # TensorRT
+                                 yolov5s.mlmodel            # CoreML (macOS-only)
+                                 yolov5s_saved_model        # TensorFlow SavedModel
+                                 yolov5s.pb                 # TensorFlow GraphDef
+                                 yolov5s.tflite             # TensorFlow Lite
+                                 yolov5s_edgetpu.tflite     # TensorFlow Edge TPU
+                                 yolov5s_paddle_model       # PaddlePaddle
+"""
+
+import argparse
+import csv
+import os
+import platform
+import sys
+from pathlib import Path
+
+import torch
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from ultralytics.utils.plotting import Annotator, colors, save_one_box
+
+from models.common import DetectMultiBackend
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+from utils.torch_utils import select_device, smart_inference_mode
+from utils.augmentations import letterbox
+
+import numpy as np
+from cv_bridge import CvBridge
+import rospy
+from sensor_msgs.msg import Image
+
+class ros_image_reader:
+
+
+
+    @smart_inference_mode()
+    def __init__(self,
+            weights=ROOT / 'yolov5s.pt',  # model path or triton URL
+            source= ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
+            data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
+            imgsz=(640, 640),  # inference size (height, width)
+            conf_thres=0.25,  # confidence threshold
+            iou_thres=0.45,  # NMS IOU threshold
+            max_det=1000,  # maximum detections per image
+            device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+            view_img=False,  # show results
+            send_ros=True,
+            save_img=False,
+            save_txt=False,  # save results to *.txt
+            save_csv=False,  # save results in CSV format
+            save_conf=False,  # save confidences in --save-txt labels
+            save_crop=False,  # save cropped prediction boxes
+            nosave=False,  # do not save images/videos
+            classes=None,  # filter by class: --class 0, or --class 0 2 3
+            agnostic_nms=False,  # class-agnostic NMS
+            augment=False,  # augmented inference
+            visualize=False,  # visualize features
+            update=False,  # update all models
+            project=ROOT / 'runs/detect',  # save results to project/name
+            name='exp',  # save results to project/name
+            exist_ok=False,  # existing project/name ok, do not increment
+            line_thickness=3,  # bounding box thickness (pixels)
+            hide_labels=False,  # hide labels
+            hide_conf=False,  # hide confidences
+            half=False,  # use FP16 half-precision inference
+            dnn=False,  # use OpenCV DNN for ONNX inference
+        ):
+        self.send_ros=send_ros
+        #Assignment
+        self.weights=weights
+        self.source= source
+        self.data=data
+        self.imgsz=imgsz
+        self.conf_thres=conf_thres
+        self.iou_thres=iou_thres
+        self.max_det=max_det
+        self.device=device
+        self.view_img=view_img
+        self.save_img=save_img
+        self.save_txt=save_txt
+        self.save_csv=save_csv
+        self.save_conf=save_conf
+        self.save_crop=save_crop
+        self.nosave=nosave
+        self.classes=classes
+        self.agnostic_nms=agnostic_nms
+        self.augment=augment
+        self.visualize=visualize
+        self.update=update
+        self.project=project
+        self.name=name
+        self.exist_ok=exist_ok
+        self.line_thickness=line_thickness
+        self.hide_labels=hide_labels
+        self.hide_conf=hide_conf
+        self.half=half
+        self.dnn=dnn
+
+
+
+
+
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/head_camera/rgb/image_raw",Image,self.callback)
+        self.image_pub = rospy.Publisher("Cole/AutoLabeledImage", Image, queue_size=2)
+        
+        source = str(source)
+
+        # Directories
+        save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+        (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+        # Load model
+        self.device = select_device(device)
+        self.model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+        self.stride, self.names, self.pt = self.model.stride, self.model.names, self.model.pt
+        self.imgsz = check_img_size(imgsz, s=self.stride)  # check image size
+
+        self.bs=1 # batch size
+
+    def callback(self,data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except Exception as e:
+            print(e)
+		
+        self.run(cv_image)
+
+    def run(self, cv_image):
+        # Run inference
+        self.model.warmup(imgsz=(1 if self.pt or self.model.triton else self.bs, 3, *self.imgsz))  # warmup
+        windows, dt = 0, [], (Profile(), Profile(), Profile())
+
+
+        # for path, im, im0s, vid_cap, s in dataset:
+        #dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
+		#The next section replaces the above line
+		#this is the section to replace when ros replaces cv2
+
+
+        img0 = cv_image  # BGR
+        # assert img0 is not None, f'Image Not Found {path}'
+        path = 'data/images/door_video.jpg'
+        s = f'image {path}: '
+
+        # Padded resize
+        img = letterbox(img0, self.imgsz, stride=self.stride, auto=self.pt)[0]
+
+        # Convert
+        img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+        img = np.ascontiguousarray(img)
+        im = img
+        im0s = img0
+        #return path, img, img0, self.cap, s
+        #END OF SECTION
+            
+        with dt[0]:
+            im = torch.from_numpy(im).to(self.model.device)
+            im = im.half() if self.model.fp16 else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+
+        # Inference
+        with dt[1]:
+            visualize = increment_path(self.save_dir / Path(path).stem, mkdir=True) if visualize else False
+            pred = self.model(im, augment=self.augment, visualize=visualize)
+
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, self.classes, self.agnostic_nms, max_det=self.max_det)
+
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+
+        # Define the path for the CSV file
+        csv_path = self.save_dir / 'predictions.csv'
+
+        # Create or append to the CSV file
+        def write_to_csv(image_name, prediction, confidence):
+            data = {'Image Name': image_name, 'Prediction': prediction, 'Confidence': confidence}
+            with open(csv_path, mode='a', newline='') as f:
+                writer = csv.DictWriter(f, fieldnames=data.keys())
+                if not csv_path.is_file():
+                    writer.writeheader()
+                writer.writerow(data)
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            
+            
+            
+
+            p = Path(p)  # to Path
+            save_path = str(self.save_dir / p.name)  # im.jpg
+            txt_path = str(self.save_dir / 'labels' / p.stem)  # im.txt
+            s += '%gx%g ' % im.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            imc = im0.copy() if self.save_crop else im0  # for save_crop
+            annotator = Annotator(im0, line_width=self.line_thickness, example=str(self.names))
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, 5].unique():
+                    n = (det[:, 5] == c).sum()  # detections per class
+                    s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    c = int(cls)  # integer class
+                    label = self.names[c] if self.hide_conf else f'{self.names[c]}'
+                    confidence = float(conf)
+                    confidence_str = f'{confidence:.2f}'
+
+                    if self.save_csv:
+                        write_to_csv(p.name, label, confidence_str)
+
+                    if self.save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if self.save_conf else (cls, *xywh)  # label format
+                        with open(f'{txt_path}.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    if self.save_img or self.save_crop or self.view_img:  # Add bbox to image
+                        c = int(cls)  # integer class
+                        label = None if self.hide_labels else (self.names[c] if self.hide_conf else f'{self.names[c]} {conf:.2f}')
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+                    if self.save_crop:
+                        save_one_box(xyxy, imc, file=self.save_dir / 'crops' / self.names[c] / f'{p.stem}.jpg', BGR=True)
+
+            # Stream results
+            im0 = annotator.result()
+            if self.view_img:
+                if platform.system() == 'Linux' and p not in windows:
+                    windows.append(p)
+                    cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
+                    cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
+                cv2.imshow(str(p), im0)
+                cv2.waitKey(1)  # 1 millisecond
+            if self.send_ros:
+                self.image_pub.publish(self.bridge.cv2_to_imgmsg(im0))
+            
+            # Save results (image with detections)
+            if self.save_img:
+                cv2.imwrite(save_path, im0)
+                
+
+        # Print time (inference-only)
+        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+
+        # Print results
+        
+        if self.save_txt or self.save_img:
+            s = f"\n{len(list(self.save_dir.glob('labels/*.txt')))} labels saved to {self.save_dir / 'labels'}" if self.save_txt else ''
+            LOGGER.info(f"Results saved to {colorstr('bold', self.save_dir)}{s}")
+        if self.update:
+            strip_optimizer(self.weights[0])  # update model (to fix SourceChangeWarning)
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path or triton URL')
+    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob/screen/0(webcam)')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='show results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-csv', action='store_true', help='save results in CSV format')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
+    opt = parser.parse_args()
+    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+    print_args(vars(opt))
+    return opt
+
+
+def main(opt):
+    check_requirements(ROOT / 'requirements.txt', exclude=('tensorboard', 'thop'))
+    detect_object = ros_image_reader(**vars(opt))
+    detect_object.run()
+
+
+if __name__ == '__main__':
+    opt = parse_opt()
+    main(opt)
