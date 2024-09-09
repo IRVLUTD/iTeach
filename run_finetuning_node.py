4ec534f8c55a4bf354058e671e7cea94ba8a9a39
jishnu@irvl-legion <legion@irvl-latop.com>
2024-09-09 16:03:54 -0500

    dip

--- run_finetuning_node.py
+++ run_finetuning_node.py
@@ -45,7 +45,8 @@ if __name__ == "__main__":
     # here only do subscription from hololens
     # get the labelled samples and save
     # trigger finetuning when received true from /hololens/out/finetune_signal
-    imgsub = receiveData.HololensUserDataSubscriber(dh_model, ros_image_reader=img_broadcaster)
+    #imgsub = receiveData.HololensUserDataSubscriber(dh_model, ros_image_reader=img_broadcaster)
+    imgsub = receiveData.HololensUserDataSubscriber(dh_model, ros_image_reader=None)
     logging.info("Hololens user data subscriber online...")
 
 
